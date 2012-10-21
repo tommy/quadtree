d6d39564939a0837a86413f8e78ab692a8319964
David Graunke <graunked@gmail.com>
2012-10-21 11:25:46 -0700

    Remove old gravitation/attraction/repulsion behaviors

--- src/behave/core.clj
+++ src/behave/core.clj
@@ -4,65 +4,14 @@
   (:use behave.goals)
   (:use behave.draw))
 
-(defn gravitate
-  [x others]
-      (let [distance (subtract {:x 200 :y 500} (:pos x) )
-          step (divide distance {:x G :y G})]
-      (move x step)))
-
-(defn distance-inverse
-  [x other]
-  (power
-   (subtract (:pos x) (:pos other))
-   {:x -1 :y -1}))
-
-(def rep 0.1)
-
-(defn repulse
-  [x other] 
-  (pos-min {:x 10 :y 10}  (multiply {:x rep :y rep} (distance-inverse x other))))
-
-(def attr -10)
-
-(defn attract
-  [x other]
-  (multiply {:x attr :y attr} (distance-inverse x other)))
-
-(defn same-color [x y] (= (:color x) (:color y)))
-
-(defn seek-similar
-  [x other]
-  (if (same-color x other)
-    (attract x other)
-    {:x 0 :y 0}))
-
-(defn fear-others
-  [x other]
-  (if (same-color x other)
-    {:x 0 :y 0}
-    (repulse x other)))
-
 (defn not-me
   [x others]
   (filter #(not (= x %)) others))
-
-(defn interact
-  [f]
-  (fn [x others]
-    (let [total-force (reduce add (map #(f x @%) others))]
-      (move x total-force))))
-
-(def seek-personal-space (interact repulse))
-
-(def socialize (interact attract))
-
-
 
 (defn random-position [x y] {:x (rand-int x) :y (rand-int y)})
 
 (defn random-map-position []
   (random-position 300 600))
-
 
 (defn wander
   [x others]
