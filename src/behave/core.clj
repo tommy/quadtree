26078a9ca94758436dcf46bfc98fae8877c30188
David Graunke <graunked@gmail.com>
2012-10-21 11:22:45 -0700

    Move fns to new language, util, and goal namespaces

--- src/behave/core.clj
+++ src/behave/core.clj
@@ -1,28 +1,8 @@
 (ns behave.core
-  (:use quil.core))
-
-(defn within-values
-  [bounds x]
-  (-> x (max (first bounds)) (min (second bounds))))
-
-(defn axis-wise
-  [f]
-  (fn [pos1 pos2]
-    {:x (f (:x pos1) (:x pos2)) :y (f (:y pos1) (:y pos2))}))
-(def add (axis-wise +))
-(def subtract (axis-wise -))
-(def multiply (axis-wise *))
-(def divide (axis-wise /))
-(def power (axis-wise #(Math/pow %1 %2)))
-(def pos-min (axis-wise min))
-(def within (axis-wise within-values))
-(defn length [pos]
-  (Math/sqrt (+ (Math/pow (:x pos) 2) (Math/pow (:y pos) 2))))
-(defn move
-  [x step]
-  (assoc x :pos (add (:pos x) step)))
-
-(def G 100)
+  (:use quil.core)
+  (:use behave.util)
+  (:use behave.goals)
+  (:use behave.draw))
 
 (defn gravitate
   [x others]
@@ -76,45 +56,13 @@
 
 (def socialize (interact attract))
 
-(defn step-towards
-  [x position]
-  (let [displacement (subtract position (:pos x))
-        magnitude (length displacement)
-        step (multiply {:x 1 :y 1} (divide displacement {:x magnitude :y magnitude}))]
-    (move x step)))
+
 
 (defn random-position [x y] {:x (rand-int x) :y (rand-int y)})
 
 (defn random-map-position []
   (random-position 300 600))
 
-(defn new-goal
-  [x]
-  (let [angle (* (rand) 2 Math/PI)
-        unit {:x (Math/cos angle) :y (Math/sin angle)}
-        distance (rand-int 50)
-        new (add (:pos x) (multiply {:x distance :y distance} unit))]
-  (assoc x :goal (within {:x [0 300] :y [0 600]} new))))
-
-(defn distance-from-goal
-  [x]
-  (abs (length (subtract (:pos x) (:goal x)))))
-
-(defn at-goal
-  [x]
-  (< (distance-from-goal x) 5))
-
-(defn seek-goal
-  [x others]
-  (if (at-goal x)
-    x
-  (step-towards x (:goal x))))
-
-(defn with-probability
-  [p f else-f]
-  (if (< (rand) p)
-    (f)
-    (else-f)))
 
 (defn wander
   [x others]
@@ -122,32 +70,7 @@
     #(new-goal x)
     #(identity x)))
 
-(defn within-radius
-  [x others r]
-  (filter #(< (length (subtract (:pos x) (:pos %))) r) others))
 
-(defn average [x y] (/ (+ x y) 2))
-
-(defn move-language-towards
-  [x other]
-  (assoc x :language (vec (map average (:language x) (:language other)))))
-
-(defn drift-language-towards-neighbors
-  [x others]
-  (let [neighbors (within-radius x others 50)]
-    (reduce move-language-towards x neighbors)))
-
-(defn mutate-language
-  [lang]
-  (with-probability 1
-    (fn [] (vec (map #(within-values [30 230] (- (+ (rand-int 3) %) 1)) lang)))
-    #(identity lang)))
-
-(defn drift-language
-  [x others]
-  (assoc x :language (mutate-language (:language x))))
-
-(defn random-language [] (vec (repeatedly 3 #(rand-int 255))))
 (defn gen-behaver
   []
   (atom {:pos (random-position 300 600)
@@ -165,30 +88,11 @@
     (doseq [behavior behaviors]
       (swap! b behavior others))))
 
-(defn setup []
-  (smooth)                          ;;Turn on anti-aliasing
-  (frame-rate 30)                    ;;Set framerate to 1 FPS
-  (background 200))                 ;;Set the background colour to
 
-(defn clear []
-  (fill 236 208 120)
-  (rect 0 0 (width) (height)))
 
 (def *shown* nil)
 
-(defn draw-behaver
-  [behaver]
-  (apply fill (:language @behaver))
-  (apply stroke (:language @behaver))
-  (if (= behaver *shown*)
-    (fill 200))
-  (let [pos (:pos @behaver)]
-    (ellipse (:x pos) (:y pos)  10 10)
-    (dotimes [m 10]
-      (apply stroke (conj (:language @behaver) (- 50 (* 50 (/ m 10)))))
-      (apply fill (conj (:language @behaver) (- 50 (* 50 (/ m 10)))))
-      (let [r (+ 20 (* 10 m))]
-        (ellipse (:x pos) (:y pos) r r)))))
+
 
 (defn behave-all
   [bs]
@@ -212,18 +116,6 @@
 
 (defn show [x]
   (def *shown* x))
-
-(defn draw []
-  (clear)
-  (stroke 0)
-  (doseq [b behavers] (draw-behaver b)))
-
-(defn run []
-  (defsketch example                  ;;Define a new sketch named example
-  :title "Oh so many grey circles"  ;;Set the title of the sketch
-  :setup setup                      ;;Specify the setup fn
-  :draw draw                        ;;Specify the draw fn
-  :size [323 200]))
 
 (def a (first behavers))
 
