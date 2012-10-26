50c1267a1469e73f17629f69a42b5d286b5b16dc
Tommy Rogers <tommysayshello@gmail.com>
2012-10-25 22:15:52 -0700

    Add tools.logging dependency.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.logging "0.2.3"]
                  [quil "1.6.0"]
                  [cljts "0.2.0"]]
                
